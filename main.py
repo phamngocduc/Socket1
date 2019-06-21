80954f4a4011c31105f8c7db6f1f47c75d1cc7bc
phamngocduc <49051124+phamngocduc@users.noreply.github.com>
2019-06-21 22:10:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from googletrans import Translator
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def translator(textmessage):
+    translator = Translator()
+    translated = translator.translate(textmessage, src='en', dest='vi')
+    return translated.text
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    #print('received my event: ' + translator(str(json)))
+    #print('received my event: ', json['message'])
+    #socketio.emit('my response', json, callback=messageReceived)
+    #json[message] = translator(str(json[message]))
+    socketio.emit('my response', json, callback=messageReceived)
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
